d499bebe59d393a31f9a6dd72288d16b04b239d6
Gradofah <88850232+Gradofah@users.noreply.github.com>
2025-01-06 18:45:43 +0300

    add: maze web game;

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Block Dash Maze-Block Dash Maze-1.2.0";
+const contentToCache = [
+    "Build/web-wv.loader.js",
+    "Build/web-wv.framework.js.unityweb",
+    "Build/web-wv.data.unityweb",
+    "Build/web-wv.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
